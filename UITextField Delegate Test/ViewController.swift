45d60548700b66ea93923c1c632044034eff8698
D7703_07 <d7703_07@d7703-07ui-imac.local>
2018-03-22 15:34:44 +0900

    UITextFeild Delegate Test

--- UITextField Delegate Test/ViewController.swift
+++ UITextField Delegate Test/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var textF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textF.placeholder = "입력하세요"
+        textF.clearButtonMode = UITextFieldViewMode.whileEditing
+        textF.borderStyle = UITextBorderStyle.line
     }
 
+    @IBAction func btn(_ sender: Any) {
+        lbl.text = "Hello " + textF.text!
+        textF.text = " "
+        
+        textF.resignFirstResponder()
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
